fb95c5f26b37d597599a97c2867b7f73fe855b75
Artiom Matievschi <Artiom.Matievschi@gmail.com>
2023-02-23 10:47:16 +0100

    Extends the script allow specifiyng the platform

--- AutomationScripts/compile.bat
+++ AutomationScripts/compile.bat
@@ -7,9 +7,12 @@ set src=%1
 @REM Read second parameter which is the location where to output built Project
 set dest=%2
 
-echo  %arg1% %arg2%
+@REM Read second parameter which is the location where to output built Project
+set platform=%3
+
+echo  %arg1% %arg2% %platform%
 
 @REM All a UAT tool provided by Unreal which builds a Win64 Project
 @REM And output its at the destination
-runUAT BuildCookRun -project="%mypath:~0,-1%/%src%" -platform=Win64 -clientconfig=DebugGame -build -cook -stage -archive -archivedirectory="%mypath:~0,-1%/%dest%"
+runUAT BuildCookRun -project="%mypath:~0,-1%/%src%" -platform=%platform% -clientconfig=DebugGame -build -cook -stage -archive -archivedirectory="%mypath:~0,-1%/%dest%" -notools
 
